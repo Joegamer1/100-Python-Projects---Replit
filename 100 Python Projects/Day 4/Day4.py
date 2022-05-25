612ccdcd5c6349e451a1374e57ae3c35895de136
Joe Belcher <41334619+Joegamer1@users.noreply.github.com>
2022-05-25 14:49:10 +0000

    added files to day 4

--- 100 Python Projects/Day 4/Day4.py
+++ 100 Python Projects/Day 4/Day4.py
@@ -15,14 +15,13 @@
 #order that things are entered can matter
 states_of_america = ['Delaware', 'Pennsylvania', 'New Jersey', 'Georgia', 'Connecticut', 'Massachusetts', 'Maryland', 'South Carolina', 'New Hampshire', 'Virginia', 'New York', 'North Carolina', 'Rhode Island', 'Vermont', 'Kentucky', 'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 'Mississippi', 'Illinois', 'Alabama', 'Maine', 'Missouri', 'Arkansas', 'Michigan', 'Florida', 'Texas', 'Iowa', 'Wisconsin', 'California', 'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 'South Dakota', 'Montana', 'Washington', 'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
 
-states_of_america[1] = "Pencilvania" #will change the 1 spot to that name eg. pennsylvania becomes pencilvania
-states_of_america.append("Joeland") #can make additions to the end of lists using append
+# states_of_america[1] = "Pencilvania" #will change the 1 spot to that name eg. pennsylvania becomes pencilvania
+# states_of_america.append("Joeland") #can make additions to the end of lists using append
 
-print(states_of_america[1]) #will print alaska
+# print(states_of_america[1]) #will print alaska
 
-print(states_of_america[2]) #will print new jersey
+# print(states_of_america[2]) #will print new jersey
 
-print(states_of_america) #prints entire list
+# print(states_of_america) #prints entire list
 
-states_of_america.append("Joeland")
-
+# print(len(states_of_america)) #length of items in list (50)
